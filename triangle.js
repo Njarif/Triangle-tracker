22daef2128d1a40c3cdb82c67244960c1ee49d53
Njarif <fmwangi38@gmail.com>
2018-12-01 14:12:33 +0300

    css edits

--- triangle.js
+++ triangle.js
@@ -5,12 +5,12 @@ var z = parseInt(prompt("enter value z:"));
 
 if(x==y && y==z){
 alert("Your triangle is an Equilateral: All sides are equal");
-}else if(x >= (y+z) || x >= (y+x) || y >= (x+z)){
-alert("That is NOT a triangle");
 }else if ((x==y && y!=z ) || (x!=y && z==x) || (z==y && z!=x)){
 alert("Your triangle is an Isosceles: Exactly 2 sides are equal");
 }else if(x!=y && y!=z && x!=z){
 alert("Your triangle is a Scalene: No sides are equal");
+}else if(x >= (y+z) || x >= (y+x) || y >= (x+z)){
+alert("That is NOT a triangle");
 }
 }
 triangletracker();

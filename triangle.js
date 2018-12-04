1fd2b9aff93bf83119da870b8a9a190aea7feb17
Njarif <fmwangi38@gmail.com>
2018-12-04 16:01:12 +0300

    added reset button

--- triangle.js
+++ triangle.js
@@ -1,16 +1,20 @@
 function triangletracker() {
-  var x = parseInt(prompt("Enter value x:"));
-  var y = parseInt(prompt("Enter value y:"));
-  var z = parseInt(prompt("Enter value z:"));
+  var x = (document.getElementById("Valuex").value);
+  var y = (document.getElementById("Valuey").value);
+  var z = (document.getElementById("Valuez").value);
+  var result;
 
   if (x == y && y == z) {
-    alert("Your triangle is an Equilateral: All sides are equal");
+    result = "Your triangle is an Equilateral: All sides are equal"
+    document.getElementById('output').innerHTML = result
   } else if ((x == y && y != z) || (x != y && z == x) || (z == y && z != x)) {
-    alert("Your triangle is an Isosceles: Exactly 2 sides are equal");
+    result = "Your triangle is an Isosceles: Exactly 2 sides are equal"
+    document.getElementById('output').innerHTML = result
   } else if (x != y && y != z && x != z) {
-    alert("Your triangle is a Scalene: No sides are equal");
+    result = "Your triangle is a Scalene: No sides are equal"
+    document.getElementById('output').innerHTML = result
   } else if (x >= (y + z) || x >= (y + x) || y >= (x + z)) {
-    alert("That is NOT a triangle");
+    result = "That is NOT a triangle"
+    document.getElementById('output').innerHTML = result
   }
 }
-triangletracker();
